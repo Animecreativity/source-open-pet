beecb37e7ff3884258dbac048d8448822e2570c9
Kitsune Mao <animecreativityfandomuwu@GMAIL.COM>
2025-03-23 05:43:51 +0300

    Add files via upload

---
+++ python/book.py
@@ -0,0 +1,24 @@
+
+def get_book_info():
+     book_info = {
+        "title": "The Helpful Fox Senko-san",
+        "author": "Rimukoro",
+        "genre": "Детская манга",
+        "summary": (
+            "Сенко-сан — это история о лисичке, "
+            "которая отправляется в увлекательное путешествие, "
+            "встречает новых друзей и преодолевает трудности. "
+            "Книга учит важным жизненным урокам о дружбе, смелости и доброте."
+        ),
+        
+        "publication_year": 2017,
+        "AnimeSeries": 2019, "12 series"
+        "namebook": "Заботливая 800-летняя жена"
+    } 
+     return book_info
+info = get_book_info()
+key = "34534675436875"        
+by = "senko-san"
+print(info)
+print(key)
+print(by)
